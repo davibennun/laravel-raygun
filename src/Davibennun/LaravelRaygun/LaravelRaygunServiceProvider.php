a6f7e27fc99c6fcc33dd99e59fe7fd5224225f4d
Peter Mein <peter@2mein.nl>
2015-06-18 15:35:13 +0200

    Updated service provider for 5.*

--- src/Davibennun/LaravelRaygun/LaravelRaygunServiceProvider.php
+++ src/Davibennun/LaravelRaygun/LaravelRaygunServiceProvider.php
@@ -7,44 +7,42 @@ use Raygun4php\RaygunClient;
 
 class LaravelRaygunServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('davibennun/laravel-raygun');
-	}
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app['raygun'] = $this->app->share(function($app)
-		{
-			return new RaygunClient(Config::get('laravel-raygun::apiKey'),Config::get('laravel-raygun::async'),Config::get('laravel-raygun::debugMode'));
-		});
-	}
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    public function boot(){
+        $this->publishes([
+            __DIR__.'/../../config/config.php' => config_path('laravel-raygun.php'),
+        ]);
+    }
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+        $this->app->singleton('RayGun', function($app){
+            return new RaygunClient(Config::get('laravel-raygun::apiKey'), Config::get('laravel-raygun::async'), Config::get('laravel-raygun::debugMode'));
+        });
+
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
